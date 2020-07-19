6d0a0423a3e3c57d683c0d0786e47dbcddf44887
Donish <nurakh99@gmail.com>
2020-07-19 15:12:02 +0500

    Admin panell set

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Genre, Movie
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ('date_created', )
+    list_display = ('title', 'nuimber_in_stock', 'daily_rate')
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
