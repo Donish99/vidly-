e0964b1711780867b233db321eb1769d93d8aa26
Donish <nurakh99@gmail.com>
2020-07-19 15:39:47 +0500

    rendering data

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+
 def index(request):
-    return  HttpResponse('Hello world')
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
+ 
